c6ce708dff025573bd84eca478ad3f09469497fd
martin.smitek <motyka.76@seznam.cz>
2021-10-01 08:28:38 +0200

    ggvb\vbh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         switch (code) {
             case "401", "402", "403" -> System.out.println("Chyba na straně klienta.");
-            case "501", "502", "503" -> System.out.println("Chyba na straně serveru.");
+            case "501", "502", "503" -> System.out.println("Chyba na straně serveru");
             default -> System.out.println("Chyba je mezi monitorem a židlí.");
 
 
