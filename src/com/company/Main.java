1b28de696ad98659e1fdcde15439d02317d16ac5
martin.smitek <motyka.76@seznam.cz>
2021-10-01 08:25:07 +0200

    ggvb\vbh

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var code = "404";
+
+        switch (code) {
+            case "401", "402", "403" -> System.out.println("Chyba na straně klienta.");
+            case "501", "502", "503" -> System.out.println("Chyba na straně serveru.");
+            default -> System.out.println("Chyba je mezi monitorem a židlí.");
+
+
+        }
+    }
+}
